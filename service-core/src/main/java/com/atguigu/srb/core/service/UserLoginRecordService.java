6279c642d8304add211bc02fd2f9f30f0fd6425e
lucky845 <lc1447545564@gmail.com>
2022-03-08 23:36:46 +0800

    会员登陆日志功能

--- service-core/src/main/java/com/atguigu/srb/core/service/UserLoginRecordService.java
+++ service-core/src/main/java/com/atguigu/srb/core/service/UserLoginRecordService.java
@@ -3,6 +3,8 @@ package com.atguigu.srb.core.service;
 import com.atguigu.srb.core.pojo.entity.UserLoginRecord;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  * 用户登录记录表 服务类
@@ -13,4 +15,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface UserLoginRecordService extends IService<UserLoginRecord> {
 
+    /**
+     * 获取会员登陆日志列表
+     * @param userId 用户id
+     */
+    List<UserLoginRecord> listTop50(Long userId);
 }
--- service-core/src/main/java/com/atguigu/srb/core/service/impl/UserLoginRecordServiceImpl.java
+++ service-core/src/main/java/com/atguigu/srb/core/service/impl/UserLoginRecordServiceImpl.java
@@ -1,10 +1,13 @@
 package com.atguigu.srb.core.service.impl;
 
-import com.atguigu.srb.core.pojo.entity.UserLoginRecord;
 import com.atguigu.srb.core.mapper.UserLoginRecordMapper;
+import com.atguigu.srb.core.pojo.entity.UserLoginRecord;
 import com.atguigu.srb.core.service.UserLoginRecordService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +20,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserLoginRecordServiceImpl extends ServiceImpl<UserLoginRecordMapper, UserLoginRecord> implements UserLoginRecordService {
 
+
+    /**
+     * 获取会员登陆日志列表
+     *
+     * @param userId 用户id
+     */
+    @Override
+    public List<UserLoginRecord> listTop50(Long userId) {
+
+        QueryWrapper<UserLoginRecord> userLoginRecordQueryWrapper = new QueryWrapper<>();
+        userLoginRecordQueryWrapper
+                .eq("user_id",userId)
+                .orderByDesc("id")
+                .last("limit 50");
+        return baseMapper.selectList(userLoginRecordQueryWrapper);
+    }
 }
